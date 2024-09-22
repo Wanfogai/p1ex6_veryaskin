56d2cdf002d600a58b65740d8c0741e3f128ad34
Wanfogai <81505429+Wanfogai@users.noreply.github.com>
2024-09-22 21:14:00 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+const Flex = () => {
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          // Try setting `flexDirection` to `"row"`.
+          flexDirection: 'column',
+        },
+      ]}>
+      <View style={{flex: 2, backgroundColor: 'red'}} />
+      <View style={{flex: 2, backgroundColor: 'white'}} />
+      <View style={{flex: 3, backgroundColor: 'blue'}} />
+      <View style={{flex: 2, backgroundColor: 'white'}} />
+      <View style={{flex: 2, backgroundColor: 'red'}} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+});
+
+export default Flex;
